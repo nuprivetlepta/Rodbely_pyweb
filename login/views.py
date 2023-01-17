05a4bd68e1f4b52553e2708e3feb1c2af220d72a
nuprivetlepta <stopkran510@gmail.com>
2023-01-17 21:05:40 +0300

    Login returns my account details, returns token.

--- login/views.py
+++ login/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.views import View
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 
 class Forma(View):
     def get(self, request):
         return render(request, "login/index.html")
+
+    def post(self, request):
+        return JsonResponse(request.POST, json_dumps_params={'indent': 4})
