2be7dc74edc8243284d0905123f75388dfde7795
nuprivetlepta <stopkran510@gmail.com>
2023-02-07 21:04:19 +0300

    Done templates from 4th practice, DRY used, tags makes head and ceiling of page copied from base.html to page index, cart.

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.views import View
-from apps.cart_shop.models import Product
+from apps.cart_shop.models import Product, CartItemShop
 
 
 class IndexShopView(View):
 
    def get(self, request):
        data = Product.objects.all()
-       context = {'data': data
+       cart_items = CartItemShop.objects.filter(cart__user=request.user)
+       context = {'data': data,
+                  'cart_items': cart_items
                   }
        return render(request, 'home/index.html', context)
